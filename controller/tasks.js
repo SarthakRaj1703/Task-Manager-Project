ecbc3ada43b79ea6a3928ee7d81db435293e6d80
Sarthak Raj <20d180031@iitb.ac.in>
2022-06-02 23:26:15 +0530

    Updated code
    API fully functional, connected to frontend.

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,24 +1,68 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req,res)=>{
-    res.send('all items');
+const getAllTasks = async (req,res)=>{
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({tasks:tasks})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 const postNewTask = async (req ,res)=> {
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
+    try{
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+    }catch(error){
+        res.status(500).json({msg:error})
+    }
+    
 }
 
-const getOneTask = (req,res)=>{
-    res.json({id:req.params.id})
+const getOneTask = async (req,res)=>{
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+    
 }
 
-const updateTask = (req,res)=>{
-    res.send('Task Updated');
+const updateTask = async (req,res)=>{
+   try{
+    const {id:taskID} = req.params
+
+   const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+       new:true,
+       runValidators:true
+   })
+   if(!task){
+    return res.status(404).json({msg:`No task with id: ${taskID}`})
+}
+    res.status(200).json({task})
+}
+   catch(error){
+    res.status(500).json({msg:error})
+   }
 }
 
-const deleteTask = (req,res)=>{
-    res.send('Task Deleted');
+const deleteTask = async (req,res)=>{
+    try{
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    }
+    catch(error){
+        res.status(500).json({msg:error})
+    }
 }
 
 
